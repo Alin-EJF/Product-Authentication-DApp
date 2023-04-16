2894159f6721093366e6537a7a040640d69ff1e6
Alin <tdgalin@gmail.com>
2023-04-16 16:52:45 +0300

    some structure, plus frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div> testing </div>
-  )
+    <div className="app">
+      <header className="header">
+        <div className="logo">Logo</div>
+        <button className="login-button">Login</button>
+      </header>
+      <div className="content-container">
+        <h1 className="main-header">Product Authentication</h1>
+        <h2 className="sub-header">Input ID or scan QR code</h2>
+        <input type="text" className="input-field" placeholder="Input ID of product" />
+        <button className="scan-button">Scan QR</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
