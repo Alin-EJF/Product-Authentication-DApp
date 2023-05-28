f329437139dd5557825ad292cae446f226570016
AlinJufa <76122532+Alintdg@users.noreply.github.com>
2023-05-28 22:22:08 +0300

    css fixes

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,15 +1,37 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import { FaHome } from "react-icons/fa";
 
 export default function Header() {
   const { user } = useContext(UserContext);
   return (
     <div className="Header">
       <header className="header">
-        <Link to={"/"} className="logo">
-          Home page
-        </Link>
+        <div className="header_links">
+          <Link to={"/"} className="header_link">
+            <FaHome
+              style={{
+                fontSize: "36px",
+                marginLeft: "18px",
+                marginRight: "40px",
+              }}
+            />
+          </Link>
+          {user?.userType === 2 && (
+            <>
+              <Link to={"/"} className="header_link">
+                Manufacturer
+              </Link>
+              <Link to={"/"} className="header_link">
+                Distributor
+              </Link>
+              <Link to={"/"} className="header_link">
+                Retailer
+              </Link>
+            </>
+          )}
+        </div>
         <Link to={user ? "/account" : "/login"} className="generic-button">
           {(!!user && <div>{user.email}</div>) || <div>Login</div>}
         </Link>
--- client/src/components/IndexPage.tsx
+++ client/src/components/IndexPage.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { UserContext } from "../UserContext";
 import ProviderForm from "./ProviderForm"; // Importing the provider form component. Please create this component.
+import { FaSearch, FaQrcode } from "react-icons/fa"; // Font Awesome icons
 
 export default function IndexPage() {
   const { user } = useContext(UserContext);
@@ -22,17 +23,34 @@ export default function IndexPage() {
           {user?.userType === 2 ? "Add new product" : "Type ID or scan QR code"}
         </h2>
         {user?.userType !== 2 && (
-          <input
-            type="text"
-            className="input-field"
-            placeholder="Type ID of product"
-          />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Type ID of product"
+            />
+            <button style={{ height: "55px", borderRadius: "0px" }}>
+              <FaSearch />
+            </button>
+          </div>
         )}
         <button
           className="generic-button"
           onClick={user?.userType === 2 ? handleAddProductClick : undefined}
         >
-          {user?.userType === 2 ? "Add Product" : "Scan QR"}
+          {user?.userType === 2 ? (
+            <>Add Product</>
+          ) : (
+            <>
+              <FaQrcode style={{ marginRight: "7px", paddingTop: "2px" }} />
+              Scan QR
+            </>
+          )}
         </button>
         {showForm && <ProviderForm />}
       </div>
