263b038ac80e52bcffb556e622dfa377f8d7c049
Alin_jf <76122532+Alintdg@users.noreply.github.com>
2023-08-23 16:05:12 +0300

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool({
   user: 'postgres',
-  password: '2750',
+  password: '',
   host: '127.0.0.1',
   port: 5432, // default PostgreSQL port
   database: 'postgres'
